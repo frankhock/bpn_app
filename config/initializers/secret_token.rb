9e7fab45075ba6554fb0b894a96a648f56097801
Frank <frank.hock@gmail.com>
2012-05-04 09:40:34 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BpnApp::Application.config.secret_token = '342b22657057c05d82343f62241ac4f37e2d8f1b45cb729fee494a0be390d6af569068f1eba66bae94ed8ae356d4a26f09278bc36b4a35b9216de7bdc8eb6bbb'
